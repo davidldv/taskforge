9f986819a79514c301fa79697bf661716d1d976e
David London <dlondon.dev@gmail.com>
2025-12-06 14:18:53 -0600

    implemented deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { PORT } from './src/config/env';
 
@@ -9,6 +10,17 @@ import connectDB from './src/database/mongodb';
 import errorMiddleware from './src/middlewares/error.middleware';
  
 const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://taskforge-front.vercel.app',
+    process.env.FRONTEND_URL
+  ].filter(Boolean) as string[],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
